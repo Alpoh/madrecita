fa1bf3c138d09caf43352b7a28ef1d3d64bc488f
Camilo Medina <medina1camilo@gmail.com>
2018-09-11 10:32:26 -0500

    linea base

---
+++ src/main/java/com/co/cm/soft/rama/RamaApplication.java
@@ -0,0 +1,12 @@
+package com.co.cm.soft.rama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamaApplication.class, args);
+	}
+}
